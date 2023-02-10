dcbf5b29ce85ea6d254f98283a478778d189343e
lmendeleeva <122634553+lmendeleeva@users.noreply.github.com>
2023-02-11 00:47:02 +0300

    Домашка с Владом

---
+++ 2.2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Console.Clear();
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a < 0)
+// {
+//     a*= -1;//a = a*-1  
+// }
+// if (a < 100)
+// {
+//     Console.WriteLine("Такой цифры нет!"); 
+//     return; //когда программа заканчивается
+// }
+// while (a > 999)
+// {
+//      a/= 10; //a = a/10
+// }
+// a %= 10; //a=a%10
+// Console.WriteLine(a);
+
+Console.Clear();
+string a = Console.ReadLine();
+if (Convert.ToString(a[0]) == "-")//строковые данные пишутся в ""
+{
+    Console.WriteLine(a[3]);   
+    return; //либо писать тут return, либо перед Console.WriteLine(a[2]) писать Else
+}
+Console.WriteLine(a[2]);
+
+
---
+++ 2.3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 7 || a < 1)
+{
+    Console.WriteLine("Нет такого дня!");
+    return;
+}
+
+switch(a)
+{
+    case 6: Console.WriteLine("Вых");
+    break;
+
+    case 7: Console.WriteLine("Вых");
+    break;
+
+    default: Console.WriteLine("Не вых");
+    break;
+
+}
+
